0084edb106643fe190992d94b415b4a980a9ea34
gerry-sheva <108265433+gerry-sheva@users.noreply.github.com>
2025-01-27 00:06:42 +0700

    refactor: use pgtype UnmarshalJSON

--- pkg/event/event.go
+++ pkg/event/event.go
@@ -2,37 +2,24 @@ package event
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/gerry-sheva/tixmaster/pkg/database/sqlc"
-	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func newEvent(ctx context.Context, dbpool *pgxpool.Pool, p *NewEventInput) error {
-	var venue_id pgtype.UUID
-	err := venue_id.Scan(p.Venue_id)
-	var host_id pgtype.UUID
-	err = host_id.Scan(p.Host_id)
-	var starting_date pgtype.Timestamptz
-	var ending_date pgtype.Timestamptz
-	err = starting_date.Scan(p.Starting_date)
-	err = ending_date.Scan(p.Ending_date)
-
 	params := sqlc.NewEventParams{
 		Name:            p.Name,
 		Summary:         p.Summary,
 		Description:     p.Description,
 		AvailableTicket: p.Available_ticket,
-		StartingDate:    starting_date,
-		EndingDate:      ending_date,
-		VenueID:         venue_id,
-		HostID:          host_id,
+		StartingDate:    p.Starting_date,
+		EndingDate:      p.Ending_date,
+		VenueID:         p.Venue_id,
+		HostID:          p.Host_id,
 	}
 
-	fmt.Printf("%v\n", venue_id)
-
-	_, err = sqlc.New(dbpool).NewEvent(ctx, params)
+	_, err := sqlc.New(dbpool).NewEvent(ctx, params)
 	if err != nil {
 		println(err.Error())
 		return err
--- pkg/event/model.go
+++ pkg/event/model.go
@@ -1,14 +1,16 @@
 package event
 
-import "time"
+import (
+	"github.com/jackc/pgx/v5/pgtype"
+)
 
 type NewEventInput struct {
-	Name             string    `json:"name"`
-	Summary          string    `json:"summary"`
-	Description      string    `json:"description"`
-	Available_ticket int32     `json:"available_ticket"`
-	Starting_date    time.Time `json:"starting_date"`
-	Ending_date      time.Time `json:"ending_date"`
-	Venue_id         string    `json:"venue_id"`
-	Host_id          string    `json:"host_id"`
+	Name             string             `json:"name"`
+	Summary          string             `json:"summary"`
+	Description      string             `json:"description"`
+	Available_ticket int32              `json:"available_ticket"`
+	Starting_date    pgtype.Timestamptz `json:"starting_date"`
+	Ending_date      pgtype.Timestamptz `json:"ending_date"`
+	Venue_id         pgtype.UUID        `json:"venue_id"`
+	Host_id          pgtype.UUID        `json:"host_id"`
 }
